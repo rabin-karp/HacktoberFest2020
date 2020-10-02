8712b4797bc9018026edfab0fdfa326ca96c0ad2
Jkjh0 <72277557+Jkjh0@users.noreply.github.com>
2020-10-03 00:45:07 +0530

    Create product4.cpp

---
+++ java/product4.cpp
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.math.*;
+	
+public class Main
+{ 
+    public static void main(String args[]) 
+    { 
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		int d = sc.nextInt();
+		System.out.println(a + " x " + b + " x " + c  + " x " + d + " = " + (a * b * c * d) );
+		
+    } 
+} 
