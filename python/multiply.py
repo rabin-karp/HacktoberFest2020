9457affb8b0c9c3e7a4b47cd20b82d9654b99d92
10doshi12 <72262399+10doshi12@users.noreply.github.com>
2021-10-02 09:55:28 +0530

    Update multiply.py

--- python/multiply.py
+++ python/multiply.py
@@ -3,7 +3,12 @@
 	1 
 	2 
 '''
-a = int(input())
-b = int(input())
-c = a * b
-print(c)
+#using try, except else will help you avoid errors
+try:
+	a = int(input('Enter the First Number For Multiplication : ')) #ask for the specified input
+	b = int(input('Enter the Second Number For Multiplication : ')) 
+except:
+	print('Invalid Input')
+else:
+	c = a * b
+	print(c)
